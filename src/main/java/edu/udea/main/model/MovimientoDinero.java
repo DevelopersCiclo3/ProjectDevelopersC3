9d4ab99ab39d4900d4b10315d8f3245f25e8035f
Jhonathan <ljhona@gmail.com>
2022-08-28 05:27:23 -0430

    1version

---
+++ src/main/java/edu/udea/main/model/MovimientoDinero.java
@@ -0,0 +1,57 @@
+package edu.udea.main.model;
+
+public class MovimientoDinero {
+    //ATRIBUTOS
+    private Double montoMovimiento;
+    private String conceptoMovimiento;
+    public Double montoPositivoNegativo;
+    public String usuarioEncargadomovimiento;
+
+    //CONSTRUCTOR
+    public MovimientoDinero(Double montoMovimiento, String conceptoMovimiento, Double montoPositivoNegativo, String usuarioEncargadomovimiento) {
+        this.montoMovimiento = montoMovimiento;
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.montoPositivoNegativo = montoPositivoNegativo;
+        this.usuarioEncargadomovimiento = usuarioEncargadomovimiento;
+    }
+
+    //GETTERS AND SETTERS
+
+    public Double getMontoMovimiento() {
+
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(Double montoMovimiento) {
+
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getConceptoMovimiento() {
+
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public Double getMontoPositivoNegativo() {
+        return montoPositivoNegativo;
+    }
+
+    public void setMontoPositivoNegativo(Double montoPositivoNegativo) {
+        this.montoPositivoNegativo = montoPositivoNegativo;
+    }
+
+    public String getUsuarioEncargadomovimiento() {
+
+        return usuarioEncargadomovimiento;
+    }
+
+    public void setUsuarioEncargadomovimiento(String usuarioEncargadomovimiento) {
+        this.usuarioEncargadomovimiento = usuarioEncargadomovimiento;
+    }
+
+}
